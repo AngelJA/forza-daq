a33ef9ba8f35547781ea424eb7f4b81e8296b3c5
Angel Avila <angel.j.avila@gmail.com>
2022-03-06 13:55:05 -0800

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+import dgram from "dgram";
+
+const server = dgram.createSocket("udp4");
+
+server.on("error", (err) => {
+  console.log(`server error:\n${err.stack}`);
+  server.close();
+});
+
+server.on("message", (msg, rinfo) => {
+  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
+
+server.on("listening", () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.bind(1642);
