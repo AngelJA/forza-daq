683ca81c3e6ec33e794c958d40cc4f3e292851e0
Angel Avila <angel.j.avila@gmail.com>
2022-03-13 06:09:24 -0700

    Add an informative warning when no data received

--- src/websocket.ts
+++ src/websocket.ts
@@ -22,6 +22,23 @@ export async function sendCommand(command: number, data?: object) {
   webSocket.send(JSON.stringify({ action: command, ...data }));
 }
 
+export function getReply(condition: (msg: any) => boolean) {
+  return new Promise((resolve) => {
+    const controller = new AbortController();
+    webSocket.addEventListener(
+      "message",
+      (event) => {
+        const msg = JSON.parse(event.data);
+        if (condition(msg)) {
+          controller.abort();
+          resolve(msg);
+        }
+      },
+      { signal: controller.signal }
+    );
+  });
+}
+
 window.addEventListener("beforeunload", () => {
   sendCommand(c.actions.endRecording);
   sendCommand(c.actions.endPlayback);
