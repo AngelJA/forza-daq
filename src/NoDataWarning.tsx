910bd4c1ad9c5a45f68ab5c3f396bc1396bed507
Angel Avila <angel.j.avila@gmail.com>
2022-03-13 06:09:24 -0700

    Add an informative warning when no data received

---
+++ src/NoDataWarning.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import './NoDataWarning.css';
+import { getReply } from './websocket';
+import c from './config.json';
+
+function NoDataWarning() {
+  const [showWarning, setShowWarning] = useState(true);
+
+  useEffect(() => {
+    getReply((msg) => msg.type === c.actions.sendGameData).then(() =>
+      setShowWarning(false),
+    );
+  }, []);
+
+  let ipAddress = window.location.hostname;
+  if (ipAddress === 'localhost') {
+    ipAddress = '127.0.0.1';
+  }
+
+  return showWarning ? (
+    <div className="NoDataDiv">
+      <div>
+        <div>
+          <p>No data recieved.</p>
+          <p>Use these settings in Forza:</p>
+          <p>Data Out: On</p>
+          <p>Data Out IP Address: {ipAddress}</p>
+          <p>Data Out UDP Port: {c.udpPort}</p>
+        </div>
+        <button type="button" onClick={() => setShowWarning(false)}>
+          ×
+        </button>
+      </div>
+    </div>
+  ) : (
+    <div />
+  );
+}
+
+export default NoDataWarning;
--- src/websocket.ts
+++ src/websocket.ts
@@ -8,6 +8,23 @@ export function sendCommand(command: number, data?: object) {
   webSocket.send(JSON.stringify({ action: command, ...data }));
 }
 
+export function getReply(condition: (msg: any) => boolean) {
+  return new Promise((resolve) => {
+    const controller = new AbortController();
+    webSocket.addEventListener(
+      'message',
+      (event) => {
+        const msg = JSON.parse(event.data);
+        if (condition(msg)) {
+          controller.abort();
+          resolve(msg);
+        }
+      },
+      { signal: controller.signal },
+    );
+  });
+}
+
 window.addEventListener('beforeunload', () => {
   sendCommand(c.actions.endRecording);
   sendCommand(c.actions.endPlayback);
