0cdabd636f44396adb9d343909858e5eec4cffea
Angel Avila <angel.j.avila@gmail.com>
2022-03-15 03:11:26 -0700

    Make responsive layout with react-grid-layout
    Also adds another chart for demonstration purposes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { Responsive, WidthProvider } from 'react-grid-layout';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
 import './App.css';
 import Map from './Map';
 import LogControls from './LogControls';
 import NoDataWarning from './NoDataWarning';
 import LineChart, { ChartConfig, getChartConfigs } from './LineChart';
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 function App() {
   const [configs, setConfigs] = useState<ChartConfig[]>([]);
@@ -14,12 +19,30 @@ function App() {
 
   return (
     <div className="App">
-      <LogControls />
       <NoDataWarning />
       <Map />
-      {configs.map((config) => (
-        <LineChart key={JSON.stringify(config)} config={config} />
-      ))}
+      <LogControls />
+      <ResponsiveGridLayout
+        className="layout"
+        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+        rowHeight={20}
+        margin={[0, 0]}
+        draggableHandle=".DragHandle"
+      >
+        <div key="a" data-grid={{ x: 0, y: 0, w: 12, h: 20 }}>
+          <Map />
+          <div className="DragHandle" />
+        </div>
+        {configs.map((config) => (
+          <div
+            key={JSON.stringify(config)}
+            data-grid={{ x: 0, y: 0, w: 12, h: 10 }}
+          >
+            <LineChart config={config} />
+            <div className="DragHandle" />
+          </div>
+        ))}
+      </ResponsiveGridLayout>
     </div>
   );
 }
